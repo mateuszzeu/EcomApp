0c074c991eb0ec693f89c105f13de88b61fbd73d
Mat <mateuszzeu@gmail.com>
2025-05-09 16:04:03 +0200

    Initial Commit

---
+++ EcomApp/EcomAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcomAppApp.swift
+//  EcomApp
+//
+//  Created by Liza on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct EcomAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
